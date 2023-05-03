8e8959cf1fe959476c3a582c39322aff812ada86
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-04 01:21:42 +0330

    added basic click command

--- verilogen/verilogen.py
+++ verilogen/verilogen.py
@@ -4,6 +4,17 @@ import click
 from random import randint
 from jinja2 import Environment, FileSystemLoader
 
+
+environment = Environment(loader=FileSystemLoader("./"))
+
+
 @click.command()
-def generate():
-    print("let's generate some verilog code")
+@click.option(
+    "--output",
+    type=click.File("w"),
+    default="out.v",
+    help="Path to the generated output verilog file",
+)
+@click.argument("template", type=click.File("r"))
+def generate(output, template):
+    print(output, template)
